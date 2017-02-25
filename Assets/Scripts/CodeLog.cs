debf6e58429523c5d8ed198d4a1451d21b36596e
Zachary <zachary625@outlook.com>
2017-02-26 02:25:38 +0800

    test code added
    predicate delegate is functional

--- Assets/Scripts/CodeLog.cs
+++ Assets/Scripts/CodeLog.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
 
 namespace com.zachary625.unity_utility
 {
@@ -18,18 +20,82 @@ namespace com.zachary625.unity_utility
             }
         }
 
-        public interface ILogTimer
+        public Time_Delegate TimeDelegate
         {
-            public System.DateTime Time;
+            get
+            {
+                return _TimeDelegate;
+            }
+
+            set
+            {
+                _TimeDelegate = value;
+            }
         }
 
-        public delegate void LogDelegate(string);
+        public Format_Delegate FormatDelegate
+        {
+            get
+            {
+                return _FormatDelegate;
+            }
+
+            set
+            {
+                _FormatDelegate = value;
+            }
+        }
+
+        public Log_Delegate LogDelegate
+        {
+            get
+            {
+                return _LogDelegate;
+            }
+
+            set
+            {
+                _LogDelegate = value;
+            }
+        }
+
+        public LogOptions Options
+        {
+            get
+            {
+                return _Options;
+            }
+
+            set
+            {
+                _Options = value;
+            }
+        }
+
+        public Predicate_Delegate PredicateDelegate
+        {
+            get
+            {
+                return _PredicateDelegate;
+            }
+
+            set
+            {
+                _PredicateDelegate = value;
+            }
+        }
+
+        public delegate bool Predicate_Delegate();
+        public delegate DateTime Time_Delegate();
+        public delegate void Log_Delegate(string logContent);
+        public delegate string Format_Delegate(Entry entry);
+
         public delegate void Code_Void();
         public delegate T Code_Return<T>();
 
         public class LogOptions
         {
-            public string CodeName;
+            public string Name;
             public bool LogBounds;
             public bool LogTime;
             public bool LogDuration;
@@ -43,19 +109,230 @@ namespace com.zachary625.unity_utility
         }
 
         public class Entry {
-            public string CodeName;
+            public string Name;
             public EntryType Type;
-            public System.DateTime Time;
-            public System.TimeSpan Duration;
+            public DateTime? Time;
+            public TimeSpan? Duration;
         }
 
-        public void Log(LogOptions options, Code_Void code)
+        private LogOptions _Options = new LogOptions();
+        private Stack<LogOptions> _OptionsStack = new Stack<LogOptions>();
+
+        private Predicate_Delegate _PredicateDelegate = () =>
         {
+            return true;
+        };
+
+        private Time_Delegate _TimeDelegate = () =>
+        {
+            return DateTime.Now;
+        };
+
+        private Format_Delegate _FormatDelegate = (entry) =>
+        {
+            string result = "";
+            switch (entry.Type)
+            {
+                case EntryType.Inbound:
+                    {
+                        result = "+ " + entry.Name;
+                        if (entry.Time.HasValue)
+                        {
+                            result += " @ " + entry.Time.ToString() + "." + entry.Time.Value.Millisecond;
+                        }
+                        break;
+                    }
+                case EntryType.Outbound:
+                    {
+                        result = "- " + entry.Name;
+                        if (entry.Time.HasValue)
+                        {
+                            result += " @ " + entry.Time.ToString()+"."+entry.Time.Value.Millisecond;
+                        }
+                        break;
+                    }
+                case EntryType.Duration:
+                    {
+                        result = "@ " + entry.Name;
+                        if (entry.Duration.HasValue)
+                        {
+                            result += " : " + entry.Duration.Value.TotalMilliseconds + " ms";
+                        }
+                        break;
+                    }
+            }
+            return result;
+        };
+
+        private Log_Delegate _LogDelegate = (text) =>
+        {
+            Debug.Log(text);
+        };
+
+        public void Log(Code_Void code, LogOptions options = null)
+        {
+            if (code == null)
+            {
+                return;
+            }
+            if (options == null)
+            {
+                options = this.Options;
+                if (options == null)
+                {
+                    code();
+                    return;
+                }
+            }
+            if (FormatDelegate == null || LogDelegate == null)
+            {
+                code();
+                return;
+            }
+            if (PredicateDelegate != null)
+            {
+                if (!PredicateDelegate())
+                {
+                    code();
+                    return;
+                }
+            }
+
+            DateTime? tick = null, tock = null;
+            if (options.LogTime || options.LogDuration)
+            {
+                if (TimeDelegate != null)
+                {
+                    tick = TimeDelegate();
+                }
+            }
+
+            if (options.LogBounds)
+            {
+                LogDelegate(FormatDelegate(new Entry()
+                {
+                    Type = EntryType.Inbound,
+                    Name = options.Name,
+                    Time = tick,
+                }));
+            }
+
+            code();
+
+            if (options.LogTime || options.LogDuration)
+            {
+                if (TimeDelegate != null)
+                {
+                    tock = TimeDelegate();
+                }
+            }
+            if (options.LogBounds)
+            {
+                LogDelegate(FormatDelegate(new Entry()
+                {
+                    Type = EntryType.Outbound,
+                    Name = options.Name,
+                    Time = tock,
+                }));
+            }
+
+
+            if (options.LogDuration)
+            {
+                if (tick.HasValue && tock.HasValue)
+                {
+                    LogDelegate(FormatDelegate(new Entry()
+                    {
+                        Type = EntryType.Duration,
+                        Name = options.Name,
+                        Duration = tock.Value - tick.Value,
+                    }));
+                }
+            }
+
+            return;
         }
 
-        public T Log<T>(LogOptions options, Code_Return<T> code)
+        public T Log<T>(Code_Return<T> code, LogOptions options = null)
         {
-            return default(T);
+            if (code == null)
+            {
+                return default(T);
+            }
+            if(options == null)
+            {
+                options = this.Options;
+                if (options == null)
+                {
+                    return code();
+                }
+            }
+            if (FormatDelegate == null || LogDelegate == null)
+            {
+                return code();
+            }
+            if (PredicateDelegate != null)
+            {
+                if (!PredicateDelegate())
+                {
+                    return code();
+                }
+            }
+
+            T result = default(T);
+            DateTime? tick = null, tock = null;
+            if (options.LogTime || options.LogDuration)
+            {
+                if (TimeDelegate != null)
+                {
+                    tick = TimeDelegate();
+                }
+            }
+
+            if (options.LogBounds)
+            {
+                LogDelegate(FormatDelegate(new Entry()
+                {
+                    Type = EntryType.Inbound,
+                    Name = options.Name,
+                    Time = tick,
+                }));
+            }
+
+            result = code();
+
+            if (options.LogTime || options.LogDuration)
+            {
+                if (TimeDelegate != null)
+                {
+                    tock = TimeDelegate();
+                }
+            }
+            if (options.LogBounds)
+            {
+                LogDelegate(FormatDelegate(new Entry()
+                {
+                    Type = EntryType.Outbound,
+                    Name = options.Name,
+                    Time = tock,
+                }));
+            }
+
+
+            if (options.LogDuration)
+            {
+                if (tick.HasValue && tock.HasValue)
+                {
+                    LogDelegate(FormatDelegate(new Entry()
+                    {
+                        Type = EntryType.Duration,
+                        Name = options.Name,
+                        Duration = tock.Value - tick.Value,
+                    }));
+                }
+            }
+
+            return result;
         }
 
     }
--- Assets/Scripts/_.cs
+++ Assets/Scripts/_.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using com.zachary625.unity_utility;
+
 public class _ : MonoBehaviour {
 
 	// Use this for initialization
@@ -12,4 +14,68 @@ public class _ : MonoBehaviour {
 	void Update () {
 	
 	}
+
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 200, 50), "Test Predicate"))
+        {
+            TestPredicate();
+        }
+        if (GUI.Button(new Rect(0, 100, 200, 50), "Test Overhead"))
+        {
+            TestOverhead();
+        }
+    }
+
+    void TestPredicate()
+    {
+        int i = 0;
+        CodeLog.I.PredicateDelegate = () =>
+        {
+            return i % 2 == 0;
+        };
+        CodeLog.I.Options.LogDuration = true;
+        for (i = 0; i < 10; i++)
+        {
+            CodeLog.I.Options.Name = "TestPredicate(" + i + ")";
+            CodeLog.I.Log(() =>
+            {
+                int shit = i * i;
+            });
+        }
+    }
+
+    void TestOverhead()
+    {
+        int loops = 1000000;
+        CodeLog.I.Log(() =>
+        {
+            for (int i = 0; i < loops; i++)
+            {
+                int shit = i * i;
+            }
+        }, new CodeLog.LogOptions()
+        {
+            Name = "TestOverhead(without log)",
+            LogDuration = true,
+        });
+
+
+        CodeLog.I.Log(() =>
+        {
+            for (int i = 0; i < loops; i++)
+            {
+                CodeLog.I.Log(() =>
+                {
+                    int shit = i * i;
+                });
+            }
+        }, new CodeLog.LogOptions()
+        {
+            Name = "TestOverhead(with log)",
+            LogDuration = true,
+        });
+
+    }
 }
