f0a9b00391a3d6a13d562735ff1a767b4b4a1bb9
JINGLE\chi.zhang <zachary625@outlook.com>
2017-09-07 16:18:03 +0800

    调整了CustomPreference的实现

--- Assets/Scripts/Editor/CustomPreference.cs
+++ Assets/Scripts/Editor/CustomPreference.cs
@@ -2,13 +2,14 @@
 using System.Collections;
 using UnityEditor;
 using System.Collections.Generic;
+using System;
 
 
 // make sure symbols are unicque across editor session
 // use symbol to control [MenuItem] to modify the editor tool bar 
 public class CustomPreference : MonoBehaviour
 {
-    private class PreferenceInfo
+    private class PreferenceInfo: IEnumerator, IEnumerable
     {
         public string Name;
         public string Description = string.Empty;
@@ -27,21 +28,163 @@ public class CustomPreference : MonoBehaviour
                 }
             }
         }
-        public bool Default;
+        private bool _Default;
+        public bool Default
+        {
+            get
+            {
+                return _Default;
+            }
+        }
+
+        private bool _Value;
+        public bool Value
+        {
+            get
+            {
+                if (SuperiorPreference == null)
+                {
+                    return _Value;
+                }
+                else
+                {
+                    return SuperiorPreference.Value && _Value;
+                }
+            }
+            set
+            {
+                _Value = value;
+            }
+        }
+
+        public bool? this[string symbol]
+        {
+            get
+            {
+                if (!symbol.StartsWith(this.SymbolSegment))
+                {
+                    return null;
+                }
+                if (symbol.Equals(this.SymbolSegment))
+                {
+                    return this.Value;
+                }
+                if (SubordinatePreferences == null)
+                {
+                    return null;
+                }
+                string leftover = symbol.Substring((symbol + SYMBOL_CONCATINATOR).Length);
+                foreach (PreferenceInfo sub in SubordinatePreferences)
+                {
+                    bool? subResult = sub[leftover];
+                    if (subResult.HasValue)
+                    {
+                        return subResult;
+                    }
+                }
+                return null;
+            }
+            set
+            {
+                if (!value.HasValue)
+                {
+                    return;
+                }
+                if (!symbol.StartsWith(this.SymbolSegment))
+                {
+                    return;
+                }
+                if (symbol.Equals(this.SymbolSegment))
+                {
+                    this.Value = value.Value;
+                    return;
+                }
+                if (SubordinatePreferences == null)
+                {
+                    return;
+                }
+                string leftover = symbol.Substring((symbol + SYMBOL_CONCATINATOR).Length);
+                foreach (PreferenceInfo sub in SubordinatePreferences)
+                {
+                    sub[leftover] = value;
+                }
+            }
+        }
+
+        public object Current
+        {
+            get
+            {
+                if (enumerateIndex.HasValue)
+                {
+                    if (SubordinatePreferences != null && 0 <= enumerateIndex.Value && enumerateIndex.Value < SubordinatePreferences.Size)
+                    {
+                        return SubordinatePreferences[enumerateIndex.Value].Current;
+                    }
+                    return this;
+                }
+                return null;
+            }
+        }
+
+        private int? enumerateIndex = null;
+
         private PreferenceInfo SuperiorPreference;
         public PreferenceInfoList SubordinatePreferences;
 
-        public PreferenceInfo(string name, string symbolSegment, string description = null, PreferenceInfoList subs = null)
+        public PreferenceInfo(string name, string symbolSegment, bool defaultValue = false, string description = null, PreferenceInfoList subs = null)
         {
             Name = name;
             SymbolSegment = symbolSegment;
             Description = description;
             SubordinatePreferences = subs;
+            _Default = defaultValue;
+            _Value = _Default;
+            
             if (SubordinatePreferences != null)
             {
                 foreach (PreferenceInfo sub in SubordinatePreferences)
                 {
                     sub.SuperiorPreference = this;
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if (!enumerateIndex.HasValue)
+            {
+                enumerateIndex = -1;
+                return true;
+            }
+
+            if (SubordinatePreferences == null || SubordinatePreferences.Size <= 0 || SubordinatePreferences.Size <= enumerateIndex)
+            {
+                return false;
+            }
+
+            if (0 <= enumerateIndex && SubordinatePreferences[enumerateIndex.Value].MoveNext())
+            {
+                return true;
+            }
+
+            enumerateIndex++;
+            return enumerateIndex < SubordinatePreferences.Size && SubordinatePreferences[enumerateIndex.Value].MoveNext();
+        }
+
+        public void Reset()
+        {
+            enumerateIndex = null;
+            if (SubordinatePreferences != null)
+            {
+                foreach (PreferenceInfo info in SubordinatePreferences)
+                {
+                    info.Reset();
                 }
             }
         }
@@ -76,26 +219,47 @@ public class CustomPreference : MonoBehaviour
 
         public void Add(string name, string symbolSegment)
         {
-            Add(name, symbolSegment, string.Empty, null);
+            Add(name, symbolSegment, false, string.Empty, null);
+        }
+
+        public void Add(string name, string symbolSegment, bool defaultValue)
+        {
+            Add(name, symbolSegment, defaultValue, string.Empty, null);
         }
 
         public void Add(string name, string symbolSegment, string description)
         {
-            Add(name, symbolSegment, description, null);
+            Add(name, symbolSegment, false, description, null);
         }
 
         public void Add(string name, string symbolSegment, PreferenceInfoList subs)
         {
-            Add(name, symbolSegment, string.Empty, subs);
+            Add(name, symbolSegment, false, string.Empty, subs);
+        }
+
+        public void Add(string name, string symbolSegment, bool defaultValue, string description)
+        {
+            Add(name, symbolSegment, defaultValue, description, null);
+        }
+
+        public void Add(string name, string symbolSegment, bool defaultValue, PreferenceInfoList subs)
+        {
+            Add(name, symbolSegment, defaultValue, string.Empty, subs);
         }
 
         public void Add(string name, string symbolSegment, string description, PreferenceInfoList subs)
+        {
+            Add(name, symbolSegment, false, description, subs);
+        }
+
+
+        public void Add(string name, string symbolSegment, bool defaultValue, string description, PreferenceInfoList subs)
         {
             if (dict.ContainsKey(symbolSegment))
             {
                 return;
             }
-            list.Add(new PreferenceInfo(name, symbolSegment, description, subs));
+            list.Add(new PreferenceInfo(name, symbolSegment, defaultValue, description, subs));
             dict[symbolSegment] = list[list.Count - 1];
         }
 
@@ -111,7 +275,7 @@ public class CustomPreference : MonoBehaviour
     private static readonly string SYMBOL_CONCATINATOR = "_";
     private static readonly string SYMBOL_PREFIX = "CUSTOM_PREF_";
 
-    private static readonly PreferenceInfo Roles = new PreferenceInfo("Roles", "ROLE", string.Empty, new PreferenceInfoList() {
+    private static readonly PreferenceInfo Roles = new PreferenceInfo("Roles", "ROLE", false, string.Empty, new PreferenceInfoList() {
         { "Manager", "MANAGER", new PreferenceInfoList() {
             { "Manager Tool 1", "TOOL1", "Tool 1 for manager." },
         } },
@@ -149,7 +313,7 @@ public class CustomPreference : MonoBehaviour
 
     private static bool loaded = false;
     private static bool saved = true;
-    private static Dictionary<string, bool> preferences = new Dictionary<string, bool>();
+    //private static Dictionary<string, bool> preferences = new Dictionary<string, bool>();
 
     private static Vector2 scrollPosition = new Vector2();
 
@@ -206,15 +370,15 @@ public class CustomPreference : MonoBehaviour
             text += " - " + info.Description;
         }
 
-        bool value = EditorGUILayout.ToggleLeft(text, preferences[info.Symbol]);
+        bool value = EditorGUILayout.ToggleLeft(text, info.Value);
         if (value)
         {
             PreferenceInfoListGUI(info.SubordinatePreferences);
         }
 
-        if (value != preferences[info.Symbol])
+        if (value != info.Value)
         {
-            preferences[info.Symbol] = value;
+            info.Value = value;
             saved = false;
         }
 
@@ -240,7 +404,7 @@ public class CustomPreference : MonoBehaviour
         {
             return;
         }
-        preferences[info.Symbol] = EditorPrefs.GetBool(info.Symbol, info.Default);
+        info.Value = EditorPrefs.GetBool(info.Symbol, info.Default);
         LoadPreferenceList(info.SubordinatePreferences);
     }
 
@@ -253,26 +417,26 @@ public class CustomPreference : MonoBehaviour
 
     private static void SavePreferences()
     {
-        foreach (KeyValuePair<string, bool> preference in preferences)
+        foreach (PreferenceInfo info in Roles)
         {
-            EditorPrefs.SetBool(preference.Key, preference.Value);
+            EditorPrefs.SetBool(info.Symbol, info.Value);
         }
         saved = true;
 
         string symbolString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
         List<string> symbols = new List<string>(symbolString.Split(';'));
-        foreach (KeyValuePair<string, bool> preference in preferences)
+        foreach (PreferenceInfo info in Roles)
         {
-            if (preference.Value)
+            if (info.Value)
             {
-                if (!symbols.Contains(preference.Key))
+                if (!symbols.Contains(info.Symbol))
                 {
-                    symbols.Add(preference.Key);
+                    symbols.Add(info.Symbol);
                 }
             }
             else
             {
-                symbols.RemoveAll((string symbol) => { return symbol.Equals(preference.Key); });
+                symbols.RemoveAll((string symbol) => { return symbol.Equals(info.Symbol); });
             }
         }
         symbolString = string.Join(";", symbols.ToArray());
